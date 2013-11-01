7cf599781b5c36ef608040f9c62779b37e42a646
febret <febret@phobos.evl.uic.edu>
2013-11-01 16:26:46 -0500

    updated physics example with shadows

--- examples/python/physics.py
+++ examples/python/physics.py
@@ -3,15 +3,27 @@ from omega import *
 from cyclops import *
 
 l1 = Light.create()
-l1.setEnabled(True)
 l1.setColor(Color(0.5,0.5,0.4,1))
-l1.setPosition(-2, 2, -4)
-l2 = Light.create()
-l2.setEnabled(True)
-l2.setColor(Color(0.4,0.4,0.5,1))
-l2.setPosition(2, 2, 4)
+l1.setAmbient(Color(0.1,0.1,0.15,1))
+l1.setPosition(-2, 2, 0)
+l1.setLightType(LightType.Directional)
+l1.setLightDirection(Vector3(0.5, 2, 2).normalize())
 
-ground = BoxShape.create(1.5, 0.1, 2)
+l2 = Light.create()
+l2.setColor(Color(0.4,0.4,0.5,1))
+l2.setPosition(2, 4, 0)
+l2.setLightType(LightType.Directional)
+l2.setLightDirection(Vector3(0.5, 2, 1).normalize())
+
+s1 = ShadowMap()
+s1.setTextureSize(2048,2048)
+l1.setShadow(s1)
+
+s4 = ShadowMap()
+s4.setTextureSize(2048,2048)
+l2.setShadow(s4)
+
+ground = BoxShape.create(10, 0.1, 10)
 ground.setPosition(0, 0, -3)
 ground.getRigidBody().initialize(RigidBodyType.Box, 0)
 ground.getRigidBody().sync()
@@ -25,6 +37,7 @@ spinner.setPosition(0, 0, -3)
 spinner.getRigidBody().initialize(RigidBodyType.Box, 0)
 spinner.getRigidBody().setUserControlled(True)
 spinner.setEffect('colored -d yellow')
+spinner.castShadow(True)
 # set the camera position
 getDefaultCamera().focusOn(ground)
 getDefaultCamera().setPosition(0, 1, 2)
@@ -38,7 +51,7 @@ def spawn():
 		box.getRigidBody().initialize(RigidBodyType.Box, 1)
 		box.getRigidBody().sync()
 		box.setEffect("textured -d cyclops/test/omega-transparent.png")
-
+		box.castShadow(True)
 spawn()
 
 # Spin the spinner!
