5fe6c889192c85f6710010a4662e1d5f921577d1
Alessandro Febretti <febret@gmail.com>
2013-11-01 19:18:28 -0500

    added .bat filex for launching some basic apps on windows installs

---
+++ orun.bat
@@ -0,0 +1,3 @@
+REM the following call starts an empty orun session.
+REM the -D . option is used to ignore the OMEGA_HOME environment variable (if present)
+.\bin\orun.exe -D .
---
+++ physics.bat
@@ -0,0 +1,3 @@
+REM the following call starts a a basic physics example
+REM the -D . option is used to ignore the OMEGA_HOME environment variable (if present)
+.\bin\orun.exe -D . -s examples/physics.py
