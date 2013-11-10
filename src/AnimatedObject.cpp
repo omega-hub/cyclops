c7fdaacea262f9ec1c092bfab146b9e4eecdc128
Alessandro Febretti <febret@gmail.com>
2013-11-09 19:36:03 -0600

    fix return value warning

--- src/AnimatedObject.cpp
+++ src/AnimatedObject.cpp
@@ -59,215 +59,216 @@ struct AnimationManagerFinder : public osg::NodeVisitor
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 AnimatedObject* AnimatedObject::create(const String& modelName)
 {
-	return new AnimatedObject(SceneManager::instance(), modelName);
+    return new AnimatedObject(SceneManager::instance(), modelName);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 AnimatedObject::AnimatedObject(SceneManager* scene, const String& modelName):
-	Entity(scene),
-		mySceneManager(scene), 
-		myOsgSwitch(NULL), 
-		myCurrentModelIndex(0),
-		myAnimationManager(NULL),
-		myAnimations(NULL),
-		myCurAnimation(NULL),
-		myNeedStartAnimation(false),
-		myCurAnimationId(-1)
+    Entity(scene),
+        mySceneManager(scene), 
+        myOsgSwitch(NULL), 
+        myCurrentModelIndex(0),
+        myAnimationManager(NULL),
+        myAnimations(NULL),
+        myCurAnimation(NULL),
+        myNeedStartAnimation(false),
+        myCurAnimationId(-1)
 {
-	myModel = scene->getModel(modelName);
+    myModel = scene->getModel(modelName);
 
-	osg::Node* osgRoot = NULL;
+    osg::Node* osgRoot = NULL;
 
-	if(myModel != NULL)
-	{
-		if(myModel->numNodes == 1)
-		{
-			// Single model asset
-			osgRoot = myModel->nodes[0];
-		}
-		else
-		{
-			// Multi model asset
-			myOsgSwitch = new osg::Switch();
-			int i = 0;
-			foreach(osg::Node* n, myModel->nodes)
-			{
-				myOsgSwitch->addChild(n);
-				myOsgSwitch->setChildValue(n, i++);
-			}
-			osgRoot = myOsgSwitch;
-		}
+    if(myModel != NULL)
+    {
+        if(myModel->numNodes == 1)
+        {
+            // Single model asset
+            osgRoot = myModel->nodes[0];
+        }
+        else
+        {
+            // Multi model asset
+            myOsgSwitch = new osg::Switch();
+            int i = 0;
+            foreach(osg::Node* n, myModel->nodes)
+            {
+                myOsgSwitch->addChild(n);
+                myOsgSwitch->setChildValue(n, i++);
+            }
+            osgRoot = myOsgSwitch;
+        }
 
-		initialize(osgRoot);
+        initialize(osgRoot);
 
-		// Traverse this entity hierarchy to find animations.
-		AnimationManagerFinder amf;
-		osgRoot->accept(amf);
-		if(amf.am != NULL)
-		{
-			myAnimationManager = amf.am;
-			myAnimations = &myAnimationManager->getAnimationList();
-			ofmsg("AnimatedObject %1%: found %2% animations.", %getName() %getNumAnimations());
+        // Traverse this entity hierarchy to find animations.
+        AnimationManagerFinder amf;
+        osgRoot->accept(amf);
+        if(amf.am != NULL)
+        {
+            myAnimationManager = amf.am;
+            myAnimations = &myAnimationManager->getAnimationList();
+            ofmsg("AnimatedObject %1%: found %2% animations.", %getName() %getNumAnimations());
 
-			// DO not play an anymation by default anymore.
-			//loopAnimation(0);
-		}
-	}
-	else
-	{
-		ofwarn("AnimatedObject::AnimatedObject: could not create static object: model not found - %1%", %modelName);
-	}
+            // DO not play an anymation by default anymore.
+            //loopAnimation(0);
+        }
+    }
+    else
+    {
+        ofwarn("AnimatedObject::AnimatedObject: could not create static object: model not found - %1%", %modelName);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::updateTraversal(const UpdateContext& context)
 {
-	myCurTime = context.time;
+    myCurTime = context.time;
 
-	if(myCurAnimation != NULL)
-	{
-		if(myNeedStartAnimation)
-		{
-			myNeedStartAnimation = false;
-			myAnimationManager->playAnimation(myCurAnimation);
-		}
-		// See if the current animation has finished playing.
-		if(!myAnimationManager->isPlaying(myCurAnimation))
-		{
-			ofmsg("AnimatedObject %1%: animation ended", %getName());
+    if(myCurAnimation != NULL)
+    {
+        if(myNeedStartAnimation)
+        {
+            myNeedStartAnimation = false;
+            myAnimationManager->playAnimation(myCurAnimation);
+        }
+        // See if the current animation has finished playing.
+        if(!myAnimationManager->isPlaying(myCurAnimation))
+        {
+            ofmsg("AnimatedObject %1%: animation ended", %getName());
 
-			myCurAnimation = NULL;
-			myCurAnimationId = -1;
-			if(myOnAnimationEnded.length() > 0)
-			{
-				SystemManager::instance()->getScriptInterpreter()->eval(myOnAnimationEnded);
-			}
-		}
-	}
-	SceneNode::updateTraversal(context);
+            myCurAnimation = NULL;
+            myCurAnimationId = -1;
+            if(myOnAnimationEnded.length() > 0)
+            {
+                SystemManager::instance()->getScriptInterpreter()->eval(myOnAnimationEnded);
+            }
+        }
+    }
+    SceneNode::updateTraversal(context);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::setCurrentModelIndex(int index)
 {
-	if(myOsgSwitch != NULL && index < getNumModels())
-	{
-		myCurrentModelIndex = index;
-		myOsgSwitch->setSingleChildOn(index);
-	}
+    if(myOsgSwitch != NULL && index < getNumModels())
+    {
+        myCurrentModelIndex = index;
+        myOsgSwitch->setSingleChildOn(index);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int AnimatedObject::getCurrentModelIndex()
 {
-	if(myOsgSwitch != NULL)
-	{
-		return myCurrentModelIndex;
-	}
-	return 0;
+    if(myOsgSwitch != NULL)
+    {
+        return myCurrentModelIndex;
+    }
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 bool AnimatedObject::hasAnimations()
 {
-	return myAnimationManager != NULL;
+    return myAnimationManager != NULL;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int AnimatedObject::getNumAnimations()
 {
-	if(hasAnimations())
-	{
-		return myAnimations->size();
-	}
-	return 0;
+    if(hasAnimations())
+    {
+        return myAnimations->size();
+    }
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::playAnimation(int id)
 {
-	if(hasAnimations())
-	{
-		if(id < getNumAnimations())
-		{
-			myCurAnimationId = id;
-			myCurAnimation = myAnimations->at(id);
-			myCurAnimation->setPlayMode(osgAnimation::Animation::ONCE);
-			myCurAnimation->setStartTime(myCurTime);
-			myNeedStartAnimation = true;
-		}
-	}
+    if(hasAnimations())
+    {
+        if(id < getNumAnimations())
+        {
+            myCurAnimationId = id;
+            myCurAnimation = myAnimations->at(id);
+            myCurAnimation->setPlayMode(osgAnimation::Animation::ONCE);
+            myCurAnimation->setStartTime(myCurTime);
+            myNeedStartAnimation = true;
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::loopAnimation(int id)
 {
-	if(hasAnimations())
-	{
-		if(id < getNumAnimations())
-		{
-			myCurAnimationId = id;
-			myCurAnimation = myAnimations->at(id);
-			myCurAnimation->setPlayMode(osgAnimation::Animation::LOOP);
-			myNeedStartAnimation = true;
-		}
-	}
+    if(hasAnimations())
+    {
+        if(id < getNumAnimations())
+        {
+            myCurAnimationId = id;
+            myCurAnimation = myAnimations->at(id);
+            myCurAnimation->setPlayMode(osgAnimation::Animation::LOOP);
+            myNeedStartAnimation = true;
+        }
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::pauseAnimation()
 {
-	if(hasAnimations() && myCurAnimation != NULL)
-	{
-		myAnimationManager->stopAnimation(myCurAnimation);
-	}
+    if(hasAnimations() && myCurAnimation != NULL)
+    {
+        myAnimationManager->stopAnimation(myCurAnimation);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::stopAllAnimations()
 {
-	if(hasAnimations())
-	{
-		myAnimationManager->stopAll();
-		myCurAnimation = NULL;
-		myCurAnimationId = -1;
-	}
+    if(hasAnimations())
+    {
+        myAnimationManager->stopAll();
+        myCurAnimation = NULL;
+        myCurAnimationId = -1;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int AnimatedObject::getCurAnimation()
 {
-	return myCurAnimationId;
+    return myCurAnimationId;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 float AnimatedObject::getAnimationLength(int id)
 {
-	if(hasAnimations() && id < getNumAnimations())
-	{
-		osgAnimation::Animation* anim = myAnimations->at(id);
-		anim->computeDuration(); // osgAnimation: Duration must be computed before calling getDuration()
-		return anim->getDuration();
-	}
-	return 0;
+    if(hasAnimations() && id < getNumAnimations())
+    {
+        osgAnimation::Animation* anim = myAnimations->at(id);
+        anim->computeDuration(); // osgAnimation: Duration must be computed before calling getDuration()
+        return anim->getDuration();
+    }
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 float AnimatedObject::getAnimationStart(int id)
 {
-	if(hasAnimations() && id < getNumAnimations())
-	{
-		osgAnimation::Animation* anim = myAnimations->at(id);
-		return anim->getDuration();
-	}
+    if(hasAnimations() && id < getNumAnimations())
+    {
+        osgAnimation::Animation* anim = myAnimations->at(id);
+        return anim->getDuration();
+    }
+    return 0.0f;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void AnimatedObject::setAnimationStart(int id, float time)
 {
-	if(hasAnimations() && id < getNumAnimations())
-	{
-		osgAnimation::Animation* anim = myAnimations->at(id);
-		anim->setStartTime(time);
-	}
+    if(hasAnimations() && id < getNumAnimations())
+    {
+        osgAnimation::Animation* anim = myAnimations->at(id);
+        anim->setStartTime(time);
+    }
 }
