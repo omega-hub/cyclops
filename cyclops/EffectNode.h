33f50157e0d700ce002bf482e79876d9b2ae953f
Alessandro Febretti <febret@gmail.com>
2013-12-02 14:31:57 -0600

    whitespace & comments

--- cyclops/EffectNode.h
+++ cyclops/EffectNode.h
@@ -1,29 +1,38 @@
-/**************************************************************************************************
+/******************************************************************************
  * THE OMEGA LIB PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2013		Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright 2010-2013		Electronic Visualization Laboratory, 
+ *							University of Illinois at Chicago
  * Authors:										
  *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2013, Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright (c) 2010-2013, Electronic Visualization Laboratory,  
+ * University of Illinois at Chicago
  * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided 
+ * with the distribution. 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************************************/
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *-----------------------------------------------------------------------------
+ * What's in this file
+ *	An osg node handling visual effects applied to entities. This class is for
+ *  internal use only.
+ ******************************************************************************/
 #ifndef __CY_EFFECT__
 #define __CY_EFFECT__
 
@@ -41,58 +50,58 @@
 #include <omegaToolkit.h>
 
 namespace cyclops {
-	using namespace omega;
-	using namespace omegaOsg;
-	class SceneManager;
-	class ShaderManager;
+    using namespace omega;
+    using namespace omegaOsg;
+    class SceneManager;
+    class ShaderManager;
 
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	class CY_API EffectNode: public osgFX::Effect
-	{
-	public:
+    ///////////////////////////////////////////////////////////////////////////
+    class CY_API EffectNode: public osgFX::Effect
+    {
+    public:
 
-	public:
-		EffectNode() {}
-		EffectNode(SceneManager* sm);
-		virtual ~EffectNode();
+    public:
+        EffectNode() {}
+        EffectNode(SceneManager* sm);
+        virtual ~EffectNode();
 
-		EffectNode(const EffectNode& copy, const osg::CopyOp& op = osg::CopyOp::SHALLOW_COPY) : osgFX::Effect(copy, op) {	}
+        EffectNode(const EffectNode& copy, const osg::CopyOp& op = osg::CopyOp::SHALLOW_COPY) : osgFX::Effect(copy, op) {	}
 
         // Effect class info
         META_Effect(cyclops, EffectNode, "EffectNode",
                     "Base effect class for cyclops effects.",
                     "Alessandro Febretti");	
 
-		virtual bool define_techniques();
+        virtual bool define_techniques();
 
-		String getDefinition() { return myDefinition; }
-		void setDefinition(const String& definition);
+        String getDefinition() { return myDefinition; }
+        void setDefinition(const String& definition);
 
-		//TODO: Is this needed?
-		SceneManager* getSceneManager() { return mySceneManager; }
+        //TODO: Is this needed?
+        SceneManager* getSceneManager() { return mySceneManager; }
 
-		Material* getMaterial(unsigned int index);
-		void addMaterial(Material* mat);
-		int getMaterialCount();
-		void clearMaterials();
+        Material* getMaterial(unsigned int index);
+        void addMaterial(Material* mat);
+        int getMaterialCount();
+        void clearMaterials();
 
-		//! @internal sets the shader manager used by this effect to manage
-		//! material shaders
-		void setShaderManager(ShaderManager* sm);
+        //! @internal sets the shader manager used by this effect to manage
+        //! material shaders
+        void setShaderManager(ShaderManager* sm);
 
-	private:
-		Ref<osgFX::Technique> myCurrentTechnique;
+    private:
+        Ref<osgFX::Technique> myCurrentTechnique;
 
-		Vector< Ref<Material> > myMaterials;
+        Vector< Ref<Material> > myMaterials;
 
-		String myDefinition;
-		SceneManager* mySceneManager;
-		ShaderManager* myShaderManager;
-	};
-	
-	///////////////////////////////////////////////////////////////////////////////////////////////
-	inline int EffectNode::getMaterialCount()
-	{ return myMaterials.size(); }
+        String myDefinition;
+        SceneManager* mySceneManager;
+        ShaderManager* myShaderManager;
+    };
+    
+    ///////////////////////////////////////////////////////////////////////////
+    inline int EffectNode::getMaterialCount()
+    { return myMaterials.size(); }
 };
 
 #endif
